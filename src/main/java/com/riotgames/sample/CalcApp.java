717f0d69437adceefd773a9048e6b227856d8f62
JunYoung <sizz__@naver.com>
2017-04-12 00:24:31 +0900

    edit pom

--- src/main/java/com/riotgames/sample/CalcApp.java
+++ src/main/java/com/riotgames/sample/CalcApp.java
@@ -14,7 +14,7 @@ public class CalcApp {
 
         return result;
     }
-
+    
     public static void main( String[] args ) throws NumberFormatException, IOException {
         final CalcApp app = new CalcApp();
         final StringBuilder outputs = new StringBuilder();
